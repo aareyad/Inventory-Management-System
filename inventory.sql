f3b9b24758ae444108e1730e11944684503126f0
akbar-reyad <aliakbar.reyad@gmail.com>
2018-02-26 09:45:18 +0600

    db updated

--- inventory.sql
+++ inventory.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2018 at 12:07 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Feb 25, 2018 at 09:35 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 5.6.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `inventory`
@@ -23,18 +25,60 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `dealer_info`
+--
+
+CREATE TABLE `dealer_info` (
+  `id` int(11) NOT NULL,
+  `dealer_name` varchar(50) NOT NULL,
+  `company_name` varchar(50) NOT NULL,
+  `contact` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `city` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `dealer_info`
+--
+
+INSERT INTO `dealer_info` (`id`, `dealer_name`, `company_name`, `contact`, `address`, `city`) VALUES
+(1, 'Hasan', 'Hasan Co & Ltd', '0123654', 'dujdhde', 'feni');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_name`
+--
+
+CREATE TABLE `product_name` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `units` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `product_name`
+--
+
+INSERT INTO `product_name` (`id`, `product_name`, `units`) VALUES
+(1, 'Rice', 'gm'),
+(2, 'banana', 'gm');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `registration`
 --
 
-CREATE TABLE IF NOT EXISTS `registration` (
-`id` int(11) NOT NULL,
+CREATE TABLE `registration` (
+  `id` int(11) NOT NULL,
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `contact` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `registration`
@@ -43,27 +87,85 @@ CREATE TABLE IF NOT EXISTS `registration` (
 INSERT INTO `registration` (`id`, `firstname`, `lastname`, `username`, `password`, `email`, `contact`) VALUES
 (1, 'Ali', 'Akbar', 'akbar.reyad', '12345', 'aliakbar.reyad@gmail.com', '01814812929'),
 (4, 'Coyon ', 'Ahmad', 'coyon', '12345', 'lajkfjk', 'erejdfkd'),
-(5, 'Tipu', 'Sultan', 'tipu', '12345', 'tipu@gmail.com', '01823965478');
+(5, 'Tipu', 'Sultan', 'tipu', '12345', 'tipu@gmail.com', '01823965478'),
+(7, 'a', 'b', 'b', 'a', 'a@hj.com', '5568952');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `units`
+--
+
+CREATE TABLE `units` (
+  `id` int(11) NOT NULL,
+  `unit` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `units`
+--
+
+INSERT INTO `units` (`id`, `unit`) VALUES
+(1, 'gm'),
+(3, 'kg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `dealer_info`
+--
+ALTER TABLE `dealer_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_name`
+--
+ALTER TABLE `product_name`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `registration`
 --
 ALTER TABLE `registration`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `units`
+--
+ALTER TABLE `units`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `dealer_info`
+--
+ALTER TABLE `dealer_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `product_name`
+--
+ALTER TABLE `product_name`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `registration`
 --
 ALTER TABLE `registration`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `units`
+--
+ALTER TABLE `units`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
